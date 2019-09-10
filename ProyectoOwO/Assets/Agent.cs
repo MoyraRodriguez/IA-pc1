9d0c180a922308c402ae64ee5d1d05f0ea47a38a
MoyraRodriguez <moyrarodriguez@gmail.com>
2019-09-09 21:34:17 -0500

    this is my pc1

---
+++ ProyectoOwO/Assets/Agent.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired=Vector3.zero;
+    Vector3 steer = Vector3.zero;
+
+    float maxSpeed = 5; 
+    float maxSteer = 5;
+    Transform target;    
+
+    private int Radius = 1;
+
+    [SerializeField]
+    private Transform Target;
+
+    private bool IsEnter = false;
+ 
+    void Update()
+    {
+        //This makes it move
+        Movement();
+        //This makes collision with center
+        Collision();
+        //This is for the borders 
+        OutOfRanges();
+
+    }
+
+    void Movement(){
+
+        target = GameObject.Find("Target").transform;
+
+        desired = -(target.position -transform.position).normalized*maxSpeed;
+        steer = Vector3.ClampMagnitude(desired-velocity, maxSteer); 
+
+    velocity += steer*Time.deltaTime;
+    transform.position += velocity*Time.deltaTime;
+    }
+
+    void Collision(){
+     float distance = (Target.position - transform.position).magnitude;
+        if (distance <= Radius)
+        {
+            if (!IsEnter)
+            {
+                IsEnter = true;
+               Destroy(gameObject);
+               print("Toco el centro");
+            }
+        }
+        else
+        {
+            if (IsEnter)
+            {
+                IsEnter = false;
+                
+            }
+        }
+    }
+
+    void OutOfRanges(){
+        if(transform.position.x >9.41f){
+            Destroy(gameObject);
+            print("se salio del borde");
+        } 
+        if(transform.position.x <-9.41f){
+             Destroy(gameObject);
+            print("se salio del borde");
+        }
+        if(transform.position.y > 5.58){
+            Destroy(gameObject);
+            print("se salio del borde");
+        }
+        if(transform.position.y <- 5.58){
+            Destroy(gameObject);
+            print("se salio del borde");
+        }
+        
+    }
+}
---
+++ ProyectoOwO/Assets/Center.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : MonoBehaviour
+{
+    public float radius = 1;
+
+//This code doesn't do a thing, I'm just too blind to see the circle without color contrart.Just ignore it please
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position,radius);
+    }
+}
